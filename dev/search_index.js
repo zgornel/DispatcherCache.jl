d9c63a0bec569632d5d526f62526c25a039fd9ff
autodocs <autodocs>
2019-02-25 11:47:38 +0000

    build based on 15435bb

--- dev/search_index.js
+++ dev/search_index.js
@@ -13,7 +13,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Introduction",
     "title": "Introduction",
     "category": "section",
-    "text": "DispatcherCache is a task persistency mechanism for Dispatcher.jl computational task graphs. It is based on graphchain."
+    "text": "DispatcherCache is a task persistency mechanism for Dispatcher.jl computational task graphs. It is based on graphchain which is a caching mechanism for Dask task graphs."
 },
 
 {
@@ -21,7 +21,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Introduction",
     "title": "Motivation",
     "category": "section",
-    "text": "Dispatcher.jl represents a convenient way of organizing i.e. scheduling complex computational workflows for asynchronous or parallel execution. Running the same workflow multiple times is not uncommon, albeit with varying input parameters or data. Depending on the type of tasks being executed, in many cases, some of the tasks remain unchanged between distinct runs: the same function is being called on identical input arguments.DispatcherCache provides a way of re-using the output of the nodes in the dispatch graph, as long as their state did not change to some unobserved one. By state it is understood the source code, arguments and the node dependencies associated with the nodes. If the state of some node does change between two consecutive executions of the graph, only it and the nodes impacted downstream (towards the leafs of the graph) are actually executed; the nodes whose state did not change are in effect (not practice) pruned from the graph, with the exception of the ones that are dependencies of nodes that have to be re-executed due to state change."
+    "text": "Dispatcher.jl represents a convenient way of organizing i.e. scheduling complex computational workflows for asynchronous or parallel execution. Running the same workflow multiple times is not uncommon, albeit with varying input parameters or data. Depending on the type of tasks being executed, in many cases, some of the tasks remain unchanged between distinct runs: the same function is being called on identical input arguments.DispatcherCache provides a way of re-using the output of the nodes in the dispatch graph, as long as their state did not change to some unobserved one. By state it is understood the source code, arguments and the node dependencies associated with the nodes. If the state of some node does change between two consecutive executions of the graph, only the node and the nodes impacted downstream (towards the leafs of the graph) are actually executed. The nodes whose state did not change are in effect (not practice) pruned from the graph, with the exception of the ones that are dependencies of nodes that have to be re-executed due to state change."
 },
 
 {
@@ -29,7 +29,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Introduction",
     "title": "Installation",
     "category": "section",
-    "text": "In the shell of choice, using$ git clone https://github.com/zgornel/DispatcherCache.jlor, inside Julia] add https://github.com/zgornel/DispatcherCache.jl#master"
+    "text": "In the shell of choice, using$ git clone https://github.com/zgornel/DispatcherCache.jlor, inside Julia] add DispatcherCacheand for the latest master branch,] add https://github.com/zgornel/DispatcherCache.jl#master"
 },
 
 {
@@ -65,19 +65,19 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "api/#DispatcherCache.add_hash_cache!-Union{Tuple{DispatchGraph}, Tuple{T}, Tuple{DispatchGraph,Array{T,1}}, Tuple{DispatchGraph,Array{T,1},Array{T,1}}} where T<:(Union{#s114, #s115} where #s115<:AbstractString where #s114<:DispatchNode)",
+    "location": "api/#DispatcherCache.add_hash_cache!",
     "page": "API Reference",
     "title": "DispatcherCache.add_hash_cache!",
-    "category": "method",
-    "text": "add_hash_cache!(graph, endpoints, uncacheable, compression=DEFAULT_COMPRESSION, cachedir=DEFAULT_CACHE_DIR)\n\nOptimizes a delayed execution graph graph::DispatchGraph by wrapping individual nodes in load-from-disk on execute-and-store wrappers depending on the state of the disk cache and of the graph. The function modifies inplace the input graph and should be called on the same unmodified graph after each execution and not on the modified graph.\n\nOnce the original graph is modified, calling run! on it will, if the cache is already present, load the top most consistent key or alternatively re-run and store the outputs of nodes which have new state.\n\nArguments\n\nexec::Executor the Dispatcher.jl executor\ngraph::DispatchGraph input dispatch graph\nendpoints::Vector{Union{DispatchNode, AbstractString}} leaf nodes for which\n\ncaching will occur; nodes that depend on these will not be cached. The nodes can be specified either by label of by the node object itself\n\nuncacheable::Vector{Union{DispatchNode, AbstractString}}` nodes that will\n\nnever be cached and will always be executed (these nodes are still hashed and their hashes influence upstream node hashes as well)\n\nKeyword arguments\n\ncompression::String enables compression of the node outputs.\n\nAvailable options are \"none\", for no compression, \"bz2\" or \"bzip2\" for BZIP compression and \"gz\" or \"gzip\" for GZIP compression\n\ncachedir::String the cache directory.\n\nNote: This function should be used with care as it modifies the input       dispatch graph. One way to handle this is to make a function that       generates the dispatch graph and calling add_hash_cache! each time on       the distict, functionally identical graphs.\n\n\n\n\n\n"
+    "category": "function",
+    "text": "add_hash_cache!(graph, endpoints=[], uncacheable=[]\n                [; compression=DEFAULT_COMPRESSION, cachedir=DEFAULT_CACHE_DIR])\n\nOptimizes a delayed execution graph graph::DispatchGraph by wrapping individual nodes in load-from-disk on execute-and-store wrappers depending on the state of the disk cache and of the graph. The function modifies inplace the input graph and should be called on the same unmodified graph after each execution and not on the modified graph.\n\nOnce the original graph is modified, calling run! on it will, if the cache is already present, load the top most consistent key or alternatively re-run and store the outputs of nodes which have new state.\n\nArguments\n\nexec::Executor the Dispatcher.jl executor\ngraph::DispatchGraph input dispatch graph\nendpoints::AbstractVector leaf nodes for which caching will occur;\n\nnodes that depend on these will not be cached. The nodes can be specified either by label of by the node object itself\n\nuncacheable::AbstractVector` nodes that will never be cached and\n\nwill always be executed (these nodes are still hashed and their hashes influence upstream node hashes as well)\n\nKeyword arguments\n\ncompression::String enables compression of the node outputs.\n\nAvailable options are \"none\", for no compression, \"bz2\" or \"bzip2\" for BZIP compression and \"gz\" or \"gzip\" for GZIP compression\n\ncachedir::String the cache directory.\n\nNote: This function should be used with care as it modifies the input       dispatch graph. One way to handle this is to make a function that       generates the dispatch graph and calling add_hash_cache! each time on       the distict, functionally identical graphs.\n\n\n\n\n\n"
 },
 
 {
-    "location": "api/#Dispatcher.run!-Union{Tuple{T}, Tuple{Executor,DispatchGraph,Array{T,1}}, Tuple{Executor,DispatchGraph,Array{T,1},Array{T,1}}} where T<:(Union{#s98, #s97} where #s97<:AbstractString where #s98<:DispatchNode)",
+    "location": "api/#Dispatcher.run!",
     "page": "API Reference",
     "title": "Dispatcher.run!",
-    "category": "method",
-    "text": "run!(exec, graph, endpoints, uncacheable=[]\n     [;compression=DEFAULT_COMPRESSION, cachedir=DEFAULT_CACHE_DIR])\n\nRuns the graph::DispatchGraph and loads or executes and stores the outputs of the nodes in the subgraph whose leaf nodes are given by endpoints. Nodes in uncachable are not locally cached.\n\nArguments\n\nexec::Executor the Dispatcher.jl executor\ngraph::DispatchGraph input dispatch graph\nendpoints::Vector{Union{DispatchNode, AbstractString}} leaf nodes for which\n\ncaching will occur; nodes that depend on these will not be cached. The nodes can be specified either by label of by the node object itself\n\nuncacheable::Vector{Union{DispatchNode, AbstractString}}` nodes that will\n\nnever be cached and will always be executed (these nodes are still hashed and their hashes influence upstream node hashes as well)\n\nKeyword arguments\n\ncompression::String enables compression of the node outputs.\n\nAvailable options are \"none\", for no compression, \"bz2\" or \"bzip2\" for BZIP compression and \"gz\" or \"gzip\" for GZIP compression\n\ncachedir::String The cache directory.\n\nExamples\n\njulia> using Dispatcher\n       using DispatcherCache\n\n       # Some functions\n       foo(x) = begin sleep(1); x end\n       bar(x) = begin sleep(1); x+1 end\n       baz(x,y) = begin sleep(1); x-y end\n\n       # Make a dispatch graph out of some operations\n       op1 = @op foo(1)\n       op2 = @op bar(2)\n       op3 = @op baz(op1, op2)\n       D = DispatchGraph(op3)\n# DispatchGraph({3, 2} directed simple Int64 graph,\n# NodeSet(DispatchNode[\n# Op(DeferredFuture at (1,1,241),baz,\"baz\"),\n# Op(DeferredFuture at (1,1,239),foo,\"foo\"),\n# Op(DeferredFuture at (1,1,240),bar,\"bar\")]))\n\njulia> # First run, writes results to disk (lasts 2 seconds)\n       result_node = [op3]  # the node for which we want results\n       cachedir = \"./__cache__\"  # directory does not exist\n       @time r = run!(AsyncExecutor(), D,\n                      result_node, cachedir=cachedir)\n       println(\"result (first run) = $(fetch(r[1].result.value))\")\n# [info | Dispatcher]: Executing 3 graph nodes.\n# [info | Dispatcher]: Node 1 (Op<baz, Op<foo>, Op<bar>>): running.\n# [info | Dispatcher]: Node 2 (Op<foo, Int64>): running.\n# [info | Dispatcher]: Node 3 (Op<bar, Int64>): running.\n# [info | Dispatcher]: Node 2 (Op<foo, Int64>): complete.\n# [info | Dispatcher]: Node 3 (Op<bar, Int64>): complete.\n# [info | Dispatcher]: Node 1 (Op<baz, Op<foo>, Op<bar>>): complete.\n# [info | Dispatcher]: All 3 nodes executed.\n#   2.029992 seconds (11.53 k allocations: 1.534 MiB)\n# result (first run) = -2\n\njulia> # Secod run, loads directly the result from ./__cache__\n       @time r = run!(AsyncExecutor(), D,\n                      [op3], cachedir=cachedir)\n       println(\"result (second run) = $(fetch(r[1].result.value))\")\n# [info | Dispatcher]: Executing 1 graph nodes.\n# [info | Dispatcher]: Node 1 (Op<baz>): running.\n# [info | Dispatcher]: Node 1 (Op<baz>): complete.\n# [info | Dispatcher]: All 1 nodes executed.\n#   0.005257 seconds (2.57 k allocations: 478.359 KiB)\n# result (second run) = -2\n\njulia> readdir(cachedir)\n# 2-element Array{String,1}:\n#  \"cache\"\n#  \"hashchain.json\"\n\n\n\n\n\n"
+    "category": "function",
+    "text": "run!(exec, graph, endpoints, uncacheable=[]\n     [;compression=DEFAULT_COMPRESSION, cachedir=DEFAULT_CACHE_DIR])\n\nRuns the graph::DispatchGraph and loads or executes and stores the outputs of the nodes in the subgraph whose leaf nodes are given by endpoints. Nodes in uncachable are not locally cached.\n\nArguments\n\nexec::Executor the Dispatcher.jl executor\ngraph::DispatchGraph input dispatch graph\nendpoints::AbstractVector leaf nodes for which caching will occur;\n\nnodes that depend on these will not be cached. The nodes can be specified either by label of by the node object itself\n\nuncacheable::AbstractVector nodes that will never be cached and will\n\nalways be executed (these nodes are still hashed and their hashes influence upstream node hashes as well)\n\nKeyword arguments\n\ncompression::String enables compression of the node outputs.\n\nAvailable options are \"none\", for no compression, \"bz2\" or \"bzip2\" for BZIP compression and \"gz\" or \"gzip\" for GZIP compression\n\ncachedir::String The cache directory.\n\nExamples\n\njulia> using Dispatcher\n       using DispatcherCache\n\n       # Some functions\n       foo(x) = begin sleep(1); x end\n       bar(x) = begin sleep(1); x+1 end\n       baz(x,y) = begin sleep(1); x-y end\n\n       # Make a dispatch graph out of some operations\n       op1 = @op foo(1)\n       op2 = @op bar(2)\n       op3 = @op baz(op1, op2)\n       D = DispatchGraph(op3)\n# DispatchGraph({3, 2} directed simple Int64 graph,\n# NodeSet(DispatchNode[\n# Op(DeferredFuture at (1,1,241),baz,\"baz\"),\n# Op(DeferredFuture at (1,1,239),foo,\"foo\"),\n# Op(DeferredFuture at (1,1,240),bar,\"bar\")]))\n\njulia> # First run, writes results to disk (lasts 2 seconds)\n       result_node = [op3]  # the node for which we want results\n       cachedir = \"./__cache__\"  # directory does not exist\n       @time r = run!(AsyncExecutor(), D,\n                      result_node, cachedir=cachedir)\n       println(\"result (first run) = $(fetch(r[1].result.value))\")\n# [info | Dispatcher]: Executing 3 graph nodes.\n# [info | Dispatcher]: Node 1 (Op<baz, Op<foo>, Op<bar>>): running.\n# [info | Dispatcher]: Node 2 (Op<foo, Int64>): running.\n# [info | Dispatcher]: Node 3 (Op<bar, Int64>): running.\n# [info | Dispatcher]: Node 2 (Op<foo, Int64>): complete.\n# [info | Dispatcher]: Node 3 (Op<bar, Int64>): complete.\n# [info | Dispatcher]: Node 1 (Op<baz, Op<foo>, Op<bar>>): complete.\n# [info | Dispatcher]: All 3 nodes executed.\n#   2.029992 seconds (11.53 k allocations: 1.534 MiB)\n# result (first run) = -2\n\njulia> # Secod run, loads directly the result from ./__cache__\n       @time r = run!(AsyncExecutor(), D,\n                      [op3], cachedir=cachedir)\n       println(\"result (second run) = $(fetch(r[1].result.value))\")\n# [info | Dispatcher]: Executing 1 graph nodes.\n# [info | Dispatcher]: Node 1 (Op<baz>): running.\n# [info | Dispatcher]: Node 1 (Op<baz>): complete.\n# [info | Dispatcher]: All 1 nodes executed.\n#   0.005257 seconds (2.57 k allocations: 478.359 KiB)\n# result (second run) = -2\n\njulia> readdir(cachedir)\n# 2-element Array{String,1}:\n#  \"cache\"\n#  \"hashchain.json\"\n\n\n\n\n\n"
 },
 
 {
@@ -110,22 +110,6 @@ var documenterSearchIndex = {"docs": [
     "title": "DispatcherCache.get_compressor",
     "category": "method",
     "text": "get_compressor(compression, action)\n\nReturn a TranscodingStreams compatible compressor or decompressor based on the values of compression and action.\n\n\n\n\n\n"
-},
-
-{
-    "location": "api/#DispatcherCache.get_dependencies-Union{Tuple{T}, Tuple{DispatchGraph,Type{T}}} where T<:DispatchNode",
-    "page": "API Reference",
-    "title": "DispatcherCache.get_dependencies",
-    "category": "method",
-    "text": "get_dependencies(graph, ::Type{T})\n\nReturns a dictionary where the keys are, depending on T, the node labels or nodes of graph::DsipatchGraph and vthe alues are iterators over either the node labels or nodes corresponding to the depencies of the key node.\n\n\n\n\n\n"
-},
-
-{
-    "location": "api/#DispatcherCache.get_keys-Union{Tuple{T}, Tuple{DispatchGraph,Type{T}}} where T<:DispatchNode",
-    "page": "API Reference",
-    "title": "DispatcherCache.get_keys",
-    "category": "method",
-    "text": "get_keys(graph, ::Type{T})\n\nReturns an iterator of the nodes of the dispatch graph graph. The returned iterator generates elements of type T: if T<:AbstractString the iterator is over the labels of the graph, if T<:DispatchNode the iterator is over the nodes of the graph.\n\n\n\n\n\n"
 },
 
 {
@@ -181,7 +165,15 @@ var documenterSearchIndex = {"docs": [
     "page": "API Reference",
     "title": "DispatcherCache.wrap_to_load!",
     "category": "method",
-    "text": "wrap_to_load!(updates, node, nodehash; cachedir=DEFAULT_CACHE_DIR, compression=DEFAULT_COMPRESSION)\n\nGenerates a new dispatch node that corresponds to node::DispatchNode and which loads a file from the cachedir cache directory whose name and extension depend on nodehash and compression and contents are the output of node. The generated node is added to updates which maps node to the generated node.\n\n\n\n\n\n"
+    "text": "wrap_to_load!(updates, node, nodehash;\n              cachedir=DEFAULT_CACHE_DIR,\n              compression=DEFAULT_COMPRESSION)\n\nGenerates a new dispatch node that corresponds to node::DispatchNode and which loads a file from the cachedir cache directory whose name and extension depend on nodehash and compression and contents are the output of node. The generated node is added to updates which maps node to the generated node.\n\n\n\n\n\n"
+},
+
+{
+    "location": "api/#DispatcherCache.wrap_to_store!-Tuple{Dict{DispatchNode,DispatchNode},DispatchNode,String}",
+    "page": "API Reference",
+    "title": "DispatcherCache.wrap_to_store!",
+    "category": "method",
+    "text": "wrap_to_store!(graph, node, nodehash;\n               cachedir=DEFAULT_CACHE_DIR,\n               compression=DEFAULT_COMPRESSION,\n               skipcache=false)\n\nGenerates a new Op node that corresponds to node::DispatchNode and which stores the output of the execution of node in a file whose name and extension depend on nodehash and compression. The generated node is added to updates which maps node to the generated node. The node output is stored in cachedir. The caching is skipped if skipcache is true.\n\n\n\n\n\n"
 },
 
 {
